9938f2518f4029b3fa3186cbd6e971aa767bba61
Igor Moura <igor.oliveira.moura@gmail.com>
2025-09-30 22:33:38 -0300

    fetch temporário

--- apps/web/server/user.ts
+++ apps/web/server/user.ts
@@ -1,4 +1,4 @@
 export const getUsers = async () => {
-  const response = await fetch(`${process.env.API_URL}/user`);
+  const response = await fetch(`http://localhost:5005/user`); // depois isso vai virar um process.env pelo amor de deus
   return response.json();
 }
