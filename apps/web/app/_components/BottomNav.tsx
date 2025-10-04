495a2566f03fd311faa12bbeb8d4e22728fdef5b
Gabriel-ADM <gabriel.admpinho@gmail.com>
2025-10-03 22:49:40 -0300

    :construction: Configuracao de rotas em botoes de nav bar

--- apps/web/app/_components/BottomNav.tsx
+++ apps/web/app/_components/BottomNav.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function BottomNav() {
     return (
         <footer className="text-white w-full h-22 fixed bottom-0 left-0 right-0 flex items-center justify-center
       bg-[#fcfcfc] border-t border-gray-200 shadow-md">
             <div className="grid grid-cols-4 items-center max-w-md mx-auto w-full gap-2">
-                <button
-                    className="flex flex-col items-center justify-center bg-white rounded-md transition gap-1"
-                >
+                <Link href={"/"} className="flex flex-col items-center justify-center bg-white rounded-md transition gap-0.5">
                     <Image
                         src="/bottom-nav/home.png"
                         alt="Home"
@@ -15,9 +14,8 @@ export default function BottomNav() {
                         height={35}
                     />
                     <span className="text-black text-center text-xs max-w-16 whitespace-normal leading-tight">Página Inicial</span>
-                </button>
-                <button
-                    className="flex flex-col items-center justify-center bg-white rounded-md transition max-w-20"
+                </Link>
+                <Link href={"/processos/meus-processos"} className="flex flex-col items-center justify-center bg-white rounded-md transition max-w-20"
                 >
                     <Image
                         src="/bottom-nav/meus-processos.png"
@@ -26,9 +24,8 @@ export default function BottomNav() {
                         height={42}
                     />
                     <span className="text-black text-center text-xs">Meus Processos</span>
-                </button>
-                <button
-                    className="flex flex-col items-center justify-center bg-white rounded-md transition max-w-20"
+                </Link>
+                <Link href={"/processos/abertos"} className="flex flex-col items-center justify-center bg-white rounded-md transition max-w-20"
                 >
                     <Image
                         src="/bottom-nav/processos-abertos.png"
@@ -37,9 +34,8 @@ export default function BottomNav() {
                         height={30}
                     />
                     <span className="text-black text-center text-xs">Processos Abertos</span>
-                </button>
-                <button
-                    className="flex flex-col items-center justify-center bg-white rounded-md transition max-w-20"
+                </Link>
+                <Link href={"/processos/finalizados"} className="flex flex-col items-center justify-center bg-white rounded-md transition max-w-20"
                 >
                     <Image
                         src="/bottom-nav/processos-finalizados.png"
@@ -48,7 +44,7 @@ export default function BottomNav() {
                         height={30}
                     />
                     <span className="text-black text-center text-xs">Processos Finalizados</span>
-                </button>
+                </Link>
             </div>
         </footer>
     );
