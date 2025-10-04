0d8f4f2925e6f8160b110b53723ca0ea73fd44a3
Igor Moura <igor.oliveira.moura@gmail.com>
2025-10-04 14:24:23 -0300

    refactor: eslint agora não tá mais marcando qualquer coisa em vermelho

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -1,34 +1,38 @@
-// @ts-check
-import eslint from '@eslint/js';
+import { ESLint } from 'eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  ignores: ['eslint.config.mjs'],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...globals.jest,
+        },
+        sourceType: 'module',
+        parserOptions: {
+          project: true,
+          tsconfigRootDir: __dirname,
+        },
       },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+      plugins: {
+        '@typescript-eslint': 'off',
+        prettier: eslintPluginPrettier,
+      },
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        'prettier/prettier': 'error',
       },
     },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-);
+  ],
+};
