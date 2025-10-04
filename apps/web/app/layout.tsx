eac099ddf5840cdccd7a4dab43522e0a0a321bfa
Gabriel-ADM <gabriel.admpinho@gmail.com>
2025-10-03 22:27:57 -0300

    :sparkles: Criacao de barra de navegacao inferior

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./_components/Header";
+import BottomNav from "./_components/BottomNav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
+        <BottomNav />
       </body>
     </html>
   );
