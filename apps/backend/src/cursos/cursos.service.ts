b19b15721810250ce89ebbd3e730eb0a57e37534
arthur <arthurdolival@gmail.com>
2025-10-05 10:46:44 -0300

    feat: cursos model now interacts with pg database

--- apps/backend/src/cursos/cursos.service.ts
+++ apps/backend/src/cursos/cursos.service.ts
@@ -1,47 +1,41 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCursoDto } from './dto/create-curso.dto';
 import { UpdateCursoDto } from './dto/update-curso.dto';
-import { Cursos } from '@repo/types';
-import { v4 as uuidv4 } from 'uuid';
+import { Curso } from './entities/curso.entity';
 
 @Injectable()
 export class CursosService {
-  private cursos: Cursos[] = [];
+  constructor(
+    @InjectRepository(Curso)
+    private readonly cursoRepository: Repository<Curso>,
+  ) {}
 
-  create(createCursoDto: CreateCursoDto): Cursos {
-
-    const novoCurso: Cursos = { 
-      ...createCursoDto,
-      id_curso: uuidv4()
-    };
-
-    this.cursos.push(novoCurso);
-    return novoCurso
+  async create(createCursoDto: CreateCursoDto): Promise<Curso> {
+    const novoCurso = this.cursoRepository.create(createCursoDto);
+    return this.cursoRepository.save(novoCurso);
   }
 
-  findAll(): Cursos[] {
-    return this.cursos;
+  async findAll(): Promise<Curso[]> {
+    return this.cursoRepository.find();
   }
 
-  findOne(id: string): Cursos {
-    const id_curso = this.cursos.find(c => c.id_curso === id);
-    if (!id_curso) throw new NotFoundException(`Curso com id ${id} não encontrado`);
-    return id_curso;
+  async findOne(id: string): Promise<Curso> {
+    const curso = await this.cursoRepository.findOne({ where: { id_curso: id } });
+    if (!curso) throw new NotFoundException(`Curso com id ${id} não encontrado`);
+    return curso;
   }
 
-  update(id: string, updateCursoDto: UpdateCursoDto): Cursos {
-    const id_curso = this.cursos.findIndex(c => c.id_curso === id);
-
-    if (!id_curso) throw new NotFoundException(`Curso com id ${id} não encontrado`);
-
-    const cursoAtualizado = {...this.cursos[id_curso], ...updateCursoDto}
-    this.cursos[id_curso] = cursoAtualizado;
-    return cursoAtualizado;
+  async update(id: string, updateCursoDto: UpdateCursoDto): Promise<Curso> {
+    const curso = await this.findOne(id);
+    Object.assign(curso, updateCursoDto);
+    return this.cursoRepository.save(curso);
   }
 
-  remove(id: string): void {
-    const index = this.cursos.findIndex(c => c.id_curso === id);
-    if (index === -1) throw new NotFoundException(`Curso com id ${id} não encontrado`);
-    this.cursos.splice(index, 1);
+  async remove(id: string): Promise<void> {
+    const result = await this.cursoRepository.delete(id);
+    if (result.affected === 0)
+      throw new NotFoundException(`Curso com id ${id} não encontrado`);
   }
 }
