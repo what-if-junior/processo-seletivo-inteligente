3b79a0b67d34c7c7bad156a931f48610d2ed484b
Igor Moura <igor.oliveira.moura@gmail.com>
2025-10-05 09:42:23 -0300

    feat(types): adicionar enums e interface de curso

--- apps/backend/src/cursos/dto/create-curso.dto.ts
+++ apps/backend/src/cursos/dto/create-curso.dto.ts
@@ -1,10 +1,10 @@
-import { Turno, Modalidade, Status_Curso } from "@repo/types";
+import { Turno, Modalidade, Status_Curso, Campus } from "@repo/types";
 
 export class CreateCursoDto {
   id_curso: string;
   nome_curso: string;
   duracao_semestres: string;
-  campus: string;
+  campus: Campus;
   modalidade: Modalidade;
   turno: Turno;
   vagas_totais: number;
