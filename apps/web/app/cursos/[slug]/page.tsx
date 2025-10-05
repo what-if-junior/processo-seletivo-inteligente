b741193c0225ce8bf35b037d8b9e773a9da7c53f
Gabriel-ADM <gabriel.admpinho@gmail.com>
2025-10-05 04:20:17 -0300

    :art: Alterando headers com titulo para componente componente de header

--- apps/web/app/cursos/[slug]/page.tsx
+++ apps/web/app/cursos/[slug]/page.tsx
@@ -2,6 +2,7 @@
 import { useRouter } from "next/navigation";
 import JobOpeningStepsTable from "../../_components/jobOpeningStepsTable";
 import { use } from 'react';
+import HeaderTitle from "../../_components/HeaderTitle";
 
 export default function Curso({ params }: { params: Promise<{ slug: string }> }) {
     const router = useRouter();
@@ -11,18 +12,7 @@ export default function Curso({ params }: { params: Promise<{ slug: string }> })
 
     return (
         <div className="mx-auto max-w-md px-6 py-4 bg-white">
-            {/* Cabecalho de curso */}
-            <div className="flex items-center mb-4 gap-1">
-                <button
-                    onClick={() => router.back()}
-                    className="flex justify-left w-12 h-12 text-3xl font-black text-[#2f9e41] transition rounded-full hover:bg-green-400"
-                >
-                    &lt;
-                </button>
-                <h1 className="text-2xl font-bold text-[#2f9e41] -mt-2">
-                    {slug.toLocaleUpperCase()}
-                </h1>
-            </div>
+            <HeaderTitle title={slug.toUpperCase()} />
 
             <JobOpeningStepsTable />
             <button
